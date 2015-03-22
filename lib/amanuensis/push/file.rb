0c24b499cc80d75b510f7caad0c3947ac94eaace
Anthony Laibe <anthony@laibe.cc>
2015-03-22 19:08:31 +0000

    Refactor file

--- lib/amanuensis/push/file.rb
+++ lib/amanuensis/push/file.rb
@@ -6,17 +6,27 @@ module Amanuensis
 
       def run(changelog)
         if ::File.exists?(FILE_NAME)
-          ::File.open(FILE_NAME, 'r') do |orig|
-            ::File.unlink(FILE_NAME)
-            ::File.open(FILE_NAME, 'w') do |new|
-              new.write changelog
-              new.write orig.read
-            end
-          end
+          prepend(changelog)
         else
-          f = ::File.new('Changelog.md', 'w')
-          f.write changelog
-          f.close
+          create(changelog)
+        end
+      end
+
+      private
+
+      def create(changelog)
+        f = ::File.new(FILE_NAME, 'w')
+        f.write changelog
+        f.close
+      end
+
+      def prepend(changelog)
+        ::File.open(FILE_NAME, 'r') do |orig|
+          ::File.unlink(FILE_NAME)
+          ::File.open(FILE_NAME, 'w') do |new|
+            new.write changelog
+            new.write orig.read
+          end
         end
       end
 
