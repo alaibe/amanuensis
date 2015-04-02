53e6b239dfbaa5c217e15d4634962e7afe56f1e6
Anthony Laibe <anthony@laibe.cc>
2015-04-02 16:17:03 +0100

    Add file spec

---
+++ spec/integrations/file_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+describe 'File Amanuensis' do
+
+  it 'Generates a file changeloge' do
+    Amanuensis.configure do |config|
+      config.push         = [:file]
+      config.code_manager = :fake
+      config.tracker      = :fake
+    end
+
+    Amanuensis.generate
+
+    contents = File.read('./Changelog.md')
+
+    expect(contents).to include 'Issues closed'
+    expect(contents).to include 'Pull requests closed'
+
+    File.unlink('./Changelog.md')
+  end
+
+end
